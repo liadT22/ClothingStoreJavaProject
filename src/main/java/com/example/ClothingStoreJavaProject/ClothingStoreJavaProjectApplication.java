9c905de6a2bace92ecf33e8685353387b13a8e01
tamaraSlu <tamaramami@gmail.com>
2023-08-30 16:07:36 +0300

    initial commit

---
+++ src/main/java/com/example/ClothingStoreJavaProject/ClothingStoreJavaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.ClothingStoreJavaProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothingStoreJavaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothingStoreJavaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ClothingStoreJavaProject/ClothingStoreJavaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ClothingStoreJavaProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothingStoreJavaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
