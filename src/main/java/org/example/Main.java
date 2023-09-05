2001028fe90419c125a499e63190de164661f783
liadT22 <liadt22@gmail.com>
2023-09-05 19:30:26 +0300

    now json will work

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import com.google.gson.Gson;
+import org.example.Classes.Employee;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Employee Liad= new Employee("1", "Liad", "111", "1", "1", "1", "1");
+        String json = new Gson().toJson(Liad);
+        System.out.println(json);
+        Employee Liad1 = new Gson().fromJson(json, Employee.class);
+        System.out.println(Liad1.getEmployeeID());
     }
 }
